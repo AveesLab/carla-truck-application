49884475dd6eca904a8f34e6e9bdd58d2fe00a56
hongkerous2619 <hongkerous2619@kookmin.ac.kr>
2025-10-21 03:29:44 -0400

    make disable to lane change under unstable gap condtion

--- truck_controller/include/truck_controller.hpp
+++ truck_controller/include/truck_controller.hpp
@@ -52,7 +52,9 @@ public:
 
 private:
     // 상수 정의
-
+    double MIN_GAP_;
+    double DESIRED_GAP_;
+    double EMERGENCY_GAP_ ;
     const double max_steer_angle_rad = 0.7;
     const double min_dist_threshold_sq = 0.01 * 0.01;
     const double TRUCK_LENGTH = 16.8;
@@ -60,6 +62,7 @@ private:
     const double SENSOR_TO_FRONT = 4.0;
     const double SENSOR_TO_REAR = 12.0;
     const double STEERING_THRESHOLD = 0.04;                  // 조향각 임계값
+
 
     const Missionidx Curve_idx_1 ={300-300,1800+300};
     const Missionidx Curve_idx_2 ={78100-300,79600+300};
@@ -144,7 +147,9 @@ private:
     bool truck1_overspeed_flag_;
     bool truck2_overspeed_flag_;
 
-
+    int LV_fid = 0;
+    int FV1_fid = 1;
+    int FV2_fid = 2;
     
 
     // 웨이포인트 관련
@@ -232,6 +237,7 @@ private:
     void check_overrun();
     void check_lane_change_end();
     void update_formation_id();
-    
+
+    bool check_stable_gaps();
 
 };
